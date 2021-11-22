1e8341d41938f526d7e2fdc4db6818fdfc684277
Leonardo Omaña <leotangram@gmail.com>
2021-11-22 12:11:03 -0500

    Config routes react-router-dom v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Router from './router/Router'
+
 function App() {
   return (
     <>
-      <h1>App.tsx</h1>
+      <Router />
     </>
   )
 }
---
+++ src/router/Router.tsx
@@ -0,0 +1,54 @@
+import {
+  BrowserRouter,
+  Navigate,
+  NavLink,
+  Route,
+  Routes
+} from 'react-router-dom'
+import logo from '../logo.svg'
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <div className="main-layout">
+        <nav>
+          <img src={logo} alt="React logo" />
+          <ul>
+            <li>
+              <NavLink
+                className={({ isActive }) => (isActive ? 'nav-active' : '')}
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) => (isActive ? 'nav-active' : '')}
+                to="/about"
+              >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) => (isActive ? 'nav-active' : '')}
+                to="/users"
+              >
+                Users
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/about" element={<h1>About page</h1>} />
+          <Route path="/users" element={<h1>Users page</h1>} />
+          <Route path="/" element={<h1>Home page</h1>} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default Router
